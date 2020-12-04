e1f74d95b27736cc227f6929e162cf622029d3eb
suhyeon3868 <suhyeon3868@naver.com>
2020-12-04 13:29:35 +0900

    assessment

---
+++ final_project/src/final_drive.py
@@ -0,0 +1,28 @@
+#!/home/pi/.pyenv/versions/rospy3/bin/python
+
+import rospy
+from geometry_msgs.msg import Twist
+from sensor_msgs.msg import LaserScan
+
+class SelfDrive:
+    def __init__(self, publisher):
+        self.publisher = publisher
+        self.count = 30
+
+    def lds_callback(self, scan):
+        print("scan[0]:", scan.ranges[0])
+        turtle_vel = Twist()
+     
+       
+        self.publisher.publish(turtle_vel)
+
+def main():
+    rospy.init_node('self_drive')
+    publisher = rospy.Publisher('cmd_vel', Twist, queue_size=1)
+    driver = SelfDrive(publisher)
+    subscriber = rospy.Subscriber('scan', LaserScan,
+                                  lambda scan: driver.lds_callback(scan))
+    rospy.spin()
+
+if __name__ == "__main__":
+    main()
